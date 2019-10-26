f2732a19fec2c82d0788c3a087e70defd9f9f268
unknown <Jawad Bhimani>
2019-10-26 18:40:03 -0400

    New email feature

---
+++ emailer.py
@@ -0,0 +1,114 @@
+import smtplib
+from smtplib import *
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
+
+
+class Emailer:
+	connected=False
+	connection=None
+	class Account():
+		def __init__(self, user_email, password):
+			self.user=user_email
+			self.password=password
+	
+	def __init__(self, user_email, password, receiver=None):
+		self.msg = MIMEMultipart("alternative")
+		self.emailer=Emailer.Account(user_email, password)
+		self.msg['From'] = user_email
+		self.receiver = receiver
+
+	def connect(self):
+		if not Emailer.connected:
+			try:
+				connection = smtplib.SMTP('smtp.gmail.com',587)
+				connection.ehlo()
+				connection.starttls()
+				connection.login(self.emailer.user,self.emailer.password)
+			except SMTPResponseException as e:
+				error_code=e.smtp_code
+				error_message=e.smtp_error
+				print('ERROR CONNECTING:'+str(error_message))
+			else:
+				Emailer.connected=True
+				Emailer.connection = connection
+
+
+	@classmethod
+	def disconnect(cls):
+		if cls.connected:
+			cls.connection.close()
+			cls.connected=False
+
+
+
+	def make_base64_attachment_email(self, file_as_base64):
+		try:
+			if not Emailer.connected:
+				self.connect()
+			self.msg['To'] = self.receiver	
+			self.msg["Subject"] = "Making sure this works"
+			part = MIMEBase("application", "octet-stream")
+			part.set_payload(file_as_base64)
+			part.add_header('Content-Transfer-Encoding', 'base64')
+			part['Content-Disposition'] = 'attachment; filename="%s"' % 'file_test.png'
+			self.msg.attach(part);
+		except:
+			self.disconnect()
+
+
+
+	def make_request_account_email(self, user_package):
+		print(user_package)
+		try:
+			if not Emailer.connected:
+				self.connect()
+			self.receiver = self.emailer.user	
+			self.msg['To'] = self.emailer.user
+			self.msg["Subject"] = "Account Request for " + str(user_package['full_name'])
+			body = 'User Package:\n'
+			body += 'Name: ' + str(user_package['full_name']) + '\n' 
+			body += 'Contact Number: ' + str(user_package['phone_number']) + '\n'
+			body += 'Email: ' + str(user_package['contact_email']) + '\n'
+			body += 'Address: ' + str(user_package['address']) + '\n'
+			body += 'About: ' + str(user_package['about']) + '\n'
+			body += 'Accept: ' + str(user_package['accept']) + '\n'
+			body += 'Deny: ' + str(user_package['deny'])
+			self.msg.attach(MIMEText(body,'plain'))
+			print("checking")
+			for file in user_package['files']:
+				part = MIMEBase("application", "octet-stream")
+				part.set_payload(file['base64'])
+				part.add_header('Content-Transfer-Encoding', 'base64')
+				part['Content-Disposition'] = 'attachment; filename="%s"' % file['fileName']
+				self.msg.attach(part)	
+		except:
+			self.disconnect()
+
+
+	def make_html_email(self):
+		try:
+			if not Emailer.connected:
+				self.connect()
+			self.msg["To"] = self.receiver
+			self.msg["Subject"] = "Making sure this works"
+			body = """
+				<b>This is an html email</b>
+				<a href="https://stackoverflow.com/questions/882712/sending-html-email-using-python">Aperta aqui viado</a>
+			"""
+			self.msg.attach(MIMEText(body,'html'))
+		except:
+			self.disconnect()		
+
+
+	def send_email(self):
+		if Emailer.connected:    
+			try:
+				Emailer.connection.sendmail(self.emailer.user, self.receiver, self.msg.as_string())
+			except SMTPRecipientsRefused as e:
+				refused = e.recipients
+				print("ERROR: "+str(e.recipients))
+			else:
+				print("EMAIL SENT SUCCESSFULLY")
---
+++ server.py
@@ -0,0 +1,30 @@
+from flask import Flask, jsonify, request
+from emailer import Emailer
+
+app = Flask(__name__)
+emailer = Emailer('ece150sucks@gmail.com', 'jawad123')
+@app.route('/test_request1', methods=['POST'])
+def test_request():
+	file = request.get_json()['file']
+	emailer.receiver = 'lnogueir@uwaterloo.ca'
+	emailer.connect()
+	emailer.make_base64_attachment_email(file)
+	emailer.send_email()
+	emailer.disconnect()
+	return jsonify({'message':'Email sent successfully'})
+
+
+@app.route('/test_request2', methods=['POST'])
+def test_request2():
+	try:
+		emailer.connect()
+		emailer.make_request_account_email(request.get_json())
+		emailer.send_email()
+		emailer.disconnect()
+		return jsonify({'message':'Email sent successfully'})		
+	except:
+		return jsonify({'error': 'Email was not sent properly'}), 500	
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
